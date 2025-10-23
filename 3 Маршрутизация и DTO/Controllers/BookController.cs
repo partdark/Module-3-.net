b5776dd2c3855487988dc59c3c52c971331679b3
User <user@example.com>
2025-10-23 17:05:59 +0500

    Выполнено задание 3 Маршрутизация и DTO, создан BookController, который имеет 5 методов

---
+++ 3 Маршрутизация и DTO/Controllers/BookController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Net;
+
+namespace _3_Маршрутизация_и_DTO.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+
+    public class BookController : ControllerBase
+    {
+        private static readonly LinkedList<Book> _books = new();
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ResponseBookDTO>> Get()
+        {
+            var books = _books.Select(b => new ResponseBookDTO(b.Id, b.Year, b.Author, b.Title));
+            return Ok(books);
+        }
+        [HttpGet("{id:guid}")]
+        public ActionResult<ResponseBookDTO> GetbyId([FromRoute] Guid id)
+        {
+            var b = _books.FirstOrDefault(b => b.Id == id);
+            if (b == null)
+                return NotFound();
+
+
+            var book =  new ResponseBookDTO(b.Id, b.Year, b.Author, b.Title);
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<ResponseBookDTO> CreateBook([FromBody] CreateBookDTO newBook)
+        {
+            var b = new Book(newBook.year, newBook.author, newBook.title);
+            _books.AddLast(b);
+
+            var book = new ResponseBookDTO(b.Id, b.Year, b.Author, b.Title);
+
+            return Ok(book);
+        }
+
+        [HttpPut("id:guid")]
+        public IActionResult Update([FromBody] UpdateBookDTO book)
+        {
+            var b = _books.FirstOrDefault(b => b.Id == book.id);
+            if (b == null)
+                return NotFound();
+            b.Year = book.year;
+            b.Author = book.author;
+            b.Title = book.title;
+
+             return NoContent();
+
+        }
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteById([FromRoute] Guid id)
+        {
+            var b = _books.FirstOrDefault(b => b.Id == id);
+            if (b == null)
+                return NotFound();
+            _books.Remove(b);
+
+            return NoContent(); 
+
+           
+        }
+    }
+}
---
+++ 3 Маршрутизация и DTO/DTO/BookDTO.cs
@@ -0,0 +1,6 @@
+﻿
+public record CreateBookDTO (int year, string author, string title);
+
+public record UpdateBookDTO(Guid id, int year, string author, string title);
+
+public record ResponseBookDTO(Guid id, int year, string author, string title);
